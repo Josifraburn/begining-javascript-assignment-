3f1b9fdcf0ba15030dc64794680bfaca15fcaa12
Josif Raburn <raburnjosif@gmail.com>
2017-12-17 17:20:28 -0700

    other commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Assignment from './stuff-with-functions.js'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Assignment />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/stuff-with-functions.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+
+var message = 'hello';
+let greeting = 'Yo!';
+const saying = 'What up?';
+
+
+var message = 'World';
+let greeting = 'Hello!';
+const saying = 'saying';
+
+
+//Assignment answers 
+//1. Var works globaly even out side a block and let works globally in a scope block 
+//2. let you can reassign and const you can not 
+//3. use const whne you know it isnt going to be changed and use let when you know it is going to need to be changed 
+
+
+console.log(message)
+console.log(greeting)
+console.log(saying)
+
+class Assignment extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default Assignment; 
